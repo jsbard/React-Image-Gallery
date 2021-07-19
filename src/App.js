cc867f2d288662ca4cb7a0a395dacc2b22b6d1b5
Andrew Hickman <hello@andrewhickman.me>
2021-07-19 18:39:35 -0400

    Remove redundant setCurrentPhotos function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+import {BrowserRouter, Route, Redirect} from "react-router-dom";
 import axios from "axios";
 import apiKey from "./config";
 import Search from "./components/Search";
@@ -16,26 +16,35 @@ class App extends Component {
 
 
     componentDidMount() {
-        this.setCurrentPhotos("skydivers");
+        this.getPhotos("skydivers");
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.state.tag !== prevState.tag && prevState !== "") {
+            this.getPhotos(this.state.tag);
+
+            window.onpopstate = e => {
+                e.preventDefault();
+                this.getPhotos(prevState.tag);
+            }
+        }
+    }
+
+
     getPhotos = (tag) => {
+        let urls = [];
         axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=24&format=json&nojsoncallback=1`)
             .then(res => res.data.photos.photo)
             .then(data => data.map(photoData => {
                 let url = `https://live.staticflickr.com/${photoData.server}/${photoData.id}_${photoData.secret}.jpg`;
-                this.setState(prevState => ({
-                    photoUrls: [...prevState.photoUrls, url]
-                }));
-            }));
-    }
-
-    setCurrentPhotos = (tag) => {
-        this.setState({
-            tag: tag,
-            photoUrls: []
-        });
-        this.getPhotos(tag);
+                urls.push(url);
+            }))
+            .then(data =>
+                this.setState({
+                    tag: tag,
+                    photoUrls: urls
+                })
+            )
     }
 
     render() {
@@ -43,15 +52,11 @@ class App extends Component {
             <BrowserRouter>
                 <div className="App">
                     <Search/>
-                    <Nav setPhotos={this.setCurrentPhotos} />
+                    <Nav currentTag={this.state.tag} setPhotos={this.getPhotos} />
+                    <PhotoContainer tag={this.state.tag} urls={this.state.photoUrls} />
 
-                    <Switch>
-                        <Route path="/" render={() => <Redirect to="/skydivers" />} />
-                        <Route path="/skydivers" render={() => <PhotoContainer tag={this.state.tag} urls={this.state.photoUrls} />} />
-                        <Route path="/desierto" render={() => <PhotoContainer tag={this.state.tag} urls={this.state.photoUrls} />} />
-                        <Route path="/お寺" render={() => <PhotoContainer tag={this.state.tag} urls={this.state.photoUrls} />} />
-                    </Switch>
-                    </div>
+                    <Route path="/" render={() => <Redirect to="/skydivers" />} />
+                </div>
             </BrowserRouter>
         );
     }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
 
 class Nav extends Component {
+
     render () {
         return (
             <nav className="main-nav">
